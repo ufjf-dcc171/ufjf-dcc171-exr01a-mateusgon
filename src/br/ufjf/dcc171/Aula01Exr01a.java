c82cc7038160f954e30083d7c40ab65d0a058afd
Mateus Gonçalo <mateus.nascimento11@gmail.com>
2017-08-08 22:44:55 -0300

    Declarando variáveis sem o parseFloat.

--- src/br/ufjf/dcc171/Aula01Exr01a.java
+++ src/br/ufjf/dcc171/Aula01Exr01a.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufjf.dcc171;
 
-/**
- *
- * @author ice
- */
+import javax.swing.JOptionPane;
+
 public class Aula01Exr01a {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        float largura;
+        float comprimento;
+        float altura;
+        String l = JOptionPane.showInputDialog("Digite a largura");
+        String c = JOptionPane.showInputDialog("Digite o comprimento");
+        Strint a = JOptionPane.showInputDialog("Digite a altura");
+        
     }
     
 }
