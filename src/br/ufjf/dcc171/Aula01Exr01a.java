094f8cc16da37168e3a8d18a549357f054f27767
Mateus Gonçalo <mateus.nascimento11@gmail.com>
2017-08-10 21:34:21 -0300

    Parsefloat e exibindo resultado em janela

--- src/br/ufjf/dcc171/Aula01Exr01a.java
+++ src/br/ufjf/dcc171/Aula01Exr01a.java
@@ -5,13 +5,16 @@ import javax.swing.JOptionPane;
 public class Aula01Exr01a {
 
     public static void main(String[] args) {
-        float largura;
-        float comprimento;
-        float altura;
+        Float largura;
+        Float comprimento;
+        Float altura;
         String l = JOptionPane.showInputDialog("Digite a largura");
         String c = JOptionPane.showInputDialog("Digite o comprimento");
-        Strint a = JOptionPane.showInputDialog("Digite a altura");
-        
+        String a = JOptionPane.showInputDialog("Digite a altura");
+        largura = Float.parseFloat(l);
+        comprimento = Float.parseFloat (c);
+        altura = Float.parseFloat (a);
+        JOptionPane.showMessageDialog(null, "O volume de " + largura + " * " + comprimento + " * " + altura + " é " + (largura * altura * comprimento) , "Resultado", JOptionPane.INFORMATION_MESSAGE);
     }
     
 }
