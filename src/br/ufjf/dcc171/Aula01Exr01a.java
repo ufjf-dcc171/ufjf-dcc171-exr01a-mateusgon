503ed31a951a8744bd999199daa9aac309c2ec22
Mateus G <Mateus G@192.168.1.4>
2017-08-12 15:44:59 -0300

    Colocando Try e Catch, finalizando versão

--- src/br/ufjf/dcc171/Aula01Exr01a.java
+++ src/br/ufjf/dcc171/Aula01Exr01a.java
@@ -5,16 +5,21 @@ import javax.swing.JOptionPane;
 public class Aula01Exr01a {
 
     public static void main(String[] args) {
+        try{
         Float largura;
         Float comprimento;
         Float altura;
-        String l = JOptionPane.showInputDialog("Digite a largura");
-        String c = JOptionPane.showInputDialog("Digite o comprimento");
-        String a = JOptionPane.showInputDialog("Digite a altura");
+        String l = JOptionPane.showInputDialog("Digite a largura em cm");
+        String c = JOptionPane.showInputDialog("Digite o comprimento em cm");
+        String a = JOptionPane.showInputDialog("Digite a altura em cm");
         largura = Float.parseFloat(l);
         comprimento = Float.parseFloat (c);
         altura = Float.parseFloat (a);
-        JOptionPane.showMessageDialog(null, "O volume de " + largura + " * " + comprimento + " * " + altura + " é " + (largura * altura * comprimento) , "Resultado", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "O volume de " + largura + " * " + comprimento + " * " + altura + " é " + (largura * altura * comprimento) + "cm³" , "Resultado", JOptionPane.INFORMATION_MESSAGE);
+        }
+        catch (NumberFormatException ex)
+        {
+            JOptionPane.showMessageDialog(null, "Digite um número no lugar correto, qualquer outro caractere não será aceito", "Entrada Inválida", JOptionPane.ERROR_MESSAGE);
+        }
     }
-    
 }
